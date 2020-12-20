8658845e47aaf9b7a24e9d068e7a762d3ec26fa3
Kronx12 <kronx12pro@gmail.com>
2020-12-20 12:05:36 +0100

    ex00 full ok

--- day07/ex00/whatever.cpp
+++ day07/ex00/whatever.cpp
@@ -6,7 +6,7 @@
 /*   By: gbaud <gbaud@42lyon.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/28 15:06:30 by gbaud             #+#    #+#             */
-/*   Updated: 2020/07/28 22:22:54 by gbaud            ###   ########lyon.fr   */
+/*   Updated: 2020/12/20 12:04:13 by gbaud            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,69 @@ void swap(T& a, T& b) {
     b = tmp;
 }
 
+// Class test
+class Awesome {
+    public:
+        Awesome( int n ) : _n( n ) {}
+        
+        bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+        bool operator!=( Awesome const & rhs ) const { return (this->_n != rhs._n); }
+        bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+        bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+        bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+        bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+        int get_n() const { return _n; }
+
+    private:
+        int _n;
+};
+
+std::ostream& operator<<(std::ostream& os, const Awesome& a) {
+    os << a.get_n();
+    return os;
+}
+// End class test
+
 int main() {
-    std::cout << "Subject Main" << std::endl;
-    int a = 2;
-    int b = 3;
-    ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    int int_a = 2;
+    int int_b = 3;
+    float float_a = 2.5f;
+    float float_b = 3.5f;
+    std::string str_a = "chaine1";
+    std::string str_b = "chaine2";
+    Awesome awesome_a(50);
+    Awesome awesome_b(100);
 
+    std::cout << "=== INT ===" << std::endl;
+    std::cout << "a = " << int_a << ", b = " << int_b << std::endl;
+    ::swap(int_a, int_b);
+    std::cout << "swap :" << std::endl;
+    std::cout << "a = " << int_a << ", b = " << int_b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(int_a, int_b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(int_a, int_b) << std::endl << std::endl;
 
+    std::cout << "=== FLOAT ===" << std::endl;
+    std::cout << "a = " << float_a << ", b = " << float_b << std::endl;
+    ::swap(float_a, float_b);
+    std::cout << "swap :" << std::endl;
+    std::cout << "a = " << float_a << ", b = " << float_b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(float_a, float_b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(float_a, float_b) << std::endl << std::endl;
+
+    std::cout << "=== STRING ===" << std::endl;
+    std::cout << "str_a = " << str_a << ", str_b = " << str_b << std::endl;
+    ::swap(str_a, str_b);
+    std::cout << "swap :" << std::endl;
+    std::cout << "str_a = " << str_a << ", str_b = " << str_b << std::endl;
+    std::cout << "min( str_a, str_b ) = " << ::min(str_a, str_b) << std::endl;
+    std::cout << "max( str_a, str_b ) = " << ::max(str_a, str_b) << std::endl << std::endl;
+
+    std::cout << "=== AWESOME ===" << std::endl;
+    std::cout << "awesome_a = " << awesome_a << ", awesome_b = " << awesome_b << std::endl;
+    ::swap(awesome_a, awesome_b);
+    std::cout << "swap :" << std::endl;
+    std::cout << "awesome_a = " << awesome_a << ", awesome_b = " << awesome_b << std::endl;
+    std::cout << "min( awesome_a, awesome_b ) = " << ::min(awesome_a, awesome_b) << std::endl;
+    std::cout << "max( awesome_a, awesome_b ) = " << ::max(awesome_a, awesome_b) << std::endl << std::endl;
     return 0;
 }
